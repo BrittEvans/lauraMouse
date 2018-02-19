c31d0c2f0443baea42d993c43600863a2a5a9185
BrittEvans <britt@ccri.com>
2018-02-18 22:54:18 -0500

    more timeplot graphs and anova

--- timePlots.r
+++ timePlots.r
@@ -5,13 +5,15 @@ library(reshape2)
 dat <- read.csv("~/lauraMouse/timeplots/allTPdata.csv")
 #dat <- dat %>% rename(Metadata_Frame = Metadata_Genotype)
 myLabels <- c("Control",expression(italic("Villin-cre, Bai1"^{"WT/Tg"})))
+#myLabels <- c("Control","Bai1")
 myLevels <- c("WT","Tg")
 myColors <- c("yellow","cyan")
-OUTDIR <- "~/lauraMouse/timeplots/"
+OUTDIR <- "~/lauraMouse/timeplots2/"
 
 myData <- dat %>%
   mutate(genotype = factor(GENOTYPE, levels = myLevels, labels=myLabels)) %>%
   rename(mouse = MOUSE.., treatment = TREATMENT) %>%
+#  filter(mouse != "BM30") %>%
   group_by(mouse,treatment,genotype,TIMEPOINT) %>%
   summarise( count = n(),
 		   area = mean(AreaShape_Area),
@@ -57,17 +59,24 @@ doTimePlots(diffGraph, "EdU Area",       "UV Area - NOUV Area", "EDU",       "ed
 doTimePlots(diffGraph, "Caspase-3 Area", "UV Area - NOUV Area", "Caspase-3", "caspaseArea.png")
 doTimePlots(diffGraph, "TUNEL Area",     "UV Area - NOUV Area", "TUNEL",     "tunelArea.png")
 
+# Plot all the data
+for (i in c("casp","caspArea","edu","eduArea","tunel","tunelArea")) {
+	for (t in c("3HR","6HR","12HR","24HR")) {
+		comparePlotsByTime(myData, i, t)
+	}
+}
 
-
-# GENOTYPE (WT/Tg), MOUSE #, TIMEPOINT (3/6/12/24HR), TREATMENT (Un/nouv)
-
-# 3 Total intensity
-# 3 Total area corrected
-# Repeat for CASPASE, TUNEL
-
-# 2-way anova using time and variable
-# And the tukey test
-
+# Do the anova's and write out the tukey's
+# TODO: Change the label to make the csv readable
+pVals <- data.frame("label"=c("genotype","TIMEPOINT","interaction"))
+writeHeader <- TRUE
+for (x in c("eduDiff","caspDiff","tunelDiff","eduAreaDiff","caspAreaDiff","tunelAreaDiff")) {
+  a <- aov(get(x) ~ genotype*TIMEPOINT, data=diffs)
+  pVals[x] <- summary(a)[[1]][["Pr(>F)"]][1:3]
+  tky = cbind(x, TukeyHSD(a)$`genotype:TIMEPOINT`)
+  write.table(tky, file=paste(OUTDIR,"/tukey.csv", sep=""), append=!writeHeader, col.names=writeHeader, quote=FALSE, sep=",")
+  writeHeader <- FALSE
+}
 
 
 doTimePlots <- function(allDiffData,myVar,myYlabel,myTitle,fileName,myDodge=0.4) {
@@ -101,4 +110,32 @@ doTimePlots <- function(allDiffData,myVar,myYlabel,myTitle,fileName,myDodge=0.4)
       scale_fill_manual(values=myColors, name="", labels=myLabels) +
       scale_colour_manual(values=myColors, name="", labels=myLabels)
       ggsave(filename=fileName, path = OUTDIR, width=10, height=7, bg="transparent")
+}
+
+
+#allData <- myData
+#colName <- "casp"
+#tp <- "3HR"
+
+comparePlotsByTime <- function(allData, colName, tp) {
+ compData <- myData %>% filter(TIMEPOINT == tp)
+ fileName <- paste(colName,tp,"png",sep=".")
+ ggplot(compData, aes(x = treatment, y = get(colName), label=mouse, colour = genotype, group = mouse, fill = genotype)) +
+   geom_line(size=1.5) +
+   geom_text(colour="black") +
+   #geom_point(colour="black",pch=21, size=5) +
+   facet_wrap(~ genotype, labeller=label_parsed) +
+   scale_colour_manual(name="", values=myColors, guide=FALSE) +
+   scale_fill_manual(name="", values=myColors, guide=FALSE) +
+    theme_classic(base_size=14) +
+    theme( axis.line.x = element_line(colour = 'black', size=0.5, linetype='solid'),
+          legend.text.align = 0,
+          axis.line.y = element_line(colour = 'black', size=0.5, linetype='solid'),
+          rect = element_rect(fill = "transparent"),
+		  strip.background = element_blank(),
+          plot.background = element_rect(fill = "transparent", colour= NA),
+          panel.background = element_rect(fill = "transparent", colour= NA),
+          legend.text=element_text(size=14)) +
+    labs(y=paste(colName,tp),x="Treatment")
+  ggsave(paste(OUTDIR, fileName, sep="/"), bg="transparent")
 }
